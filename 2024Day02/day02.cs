1fb1f830f3733c5512d961f000fae9dc3e0de7bf
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-02 22:41:52 +0100

    day 02 part 1 complete

---
+++ 2024Day02/day02.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class day02
+{
+    static void Main(string[] args)
+    {
+        string filePath = "data.txt";
+        // string filePath = "testdata.txt";
+        int cnt = 0;
+
+        var dataList = ReadFileToList(filePath);
+
+        foreach (string d in dataList)
+        {
+            // Console.WriteLine(d);
+            if (CheckLine(d)) cnt++;
+            // Console.WriteLine(cnt);
+        }
+        Console.WriteLine($"Result part1: {cnt}");
+    }
+
+    static bool CheckLine(string _line)
+    {
+        string[] lineStringList = _line.Split(' ');
+
+        bool returnValue = true;
+
+        string sortType = "";
+
+        int diff = 0;
+
+        List<int> lineIntList = lineStringList.Select(int.Parse).ToList();
+
+        for (int ix = 1; ix < lineIntList.Count; ix++)
+        {
+            diff = lineIntList[ix] - lineIntList[ix - 1];
+
+            if (Math.Abs(diff) < 1 || Math.Abs(diff) > 3)
+            {
+                return false;
+            }
+            else if (diff > 0)
+            {
+                // Console.WriteLine($"asc diff {diff}");
+                if (sortType == "" || sortType == "asc") sortType = "asc"; else return false;
+            }
+            else if (diff < 0)
+            {
+                // Console.WriteLine($"desc diff {diff}");
+                if (sortType == "" || sortType == "desc") sortType = "desc"; else return false;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return returnValue;
+    }
+
+    static List<string> ReadFileToList(string filePath)
+    {
+        // Läs alla rader från filen
+        var lines = new List<string>();
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            lines.Add(line.Trim());
+        }
+
+        return lines;
+    }
+}
---
+++ _template.cs
@@ -0,0 +1,27 @@
+﻿class day02
+{
+    static void Main(string[] args)
+    {
+        // string filePath = "data.txt";
+        string filePath = "testdata.txt";
+
+        var dataList = ReadFileToList(filePath);
+
+        foreach (string d in dataList)
+        {
+            Console.WriteLine(d);
+        }
+    }
+    static List<string> ReadFileToList(string filePath)
+    {
+        // Läs alla rader från filen
+        var lines = new List<string>();
+
+        foreach (var line in File.ReadLines(filePath))
+        {
+            lines.Add(line.Trim());
+        }
+
+        return lines;
+    }
+}
