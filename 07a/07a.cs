a78ebeb5deffc9e918f4444ce31689be999c8fbf
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-19 16:36:36 +0100

    07a completed

--- 07a/07a.cs
+++ 07a/07a.cs
@@ -16,12 +16,70 @@ static void print(string sText)
     Console.WriteLine(sText);
 }
 
-// string sFilePath = "data.txt";
-string sFilePath = "testdata.txt";
+static ulong CalculateString(string _input, bool _bDebug = false)
+{
+
+    ulong iReturnValue = 0;
+    List<ulong> iResultList = new List<ulong>();
+
+    string[] sSplitInputArray = _input.Split(':');
+    string[] sNumbersArray = sSplitInputArray[1].Trim().Split(' ');
+    string sResultList = "";
+    ulong iTestValue = ulong.Parse(sSplitInputArray[0]);
+
+    foreach (string sN in sNumbersArray)
+    {
+        iResultList = UpdateResultList(iResultList, ulong.Parse(sN));
+        if (_bDebug) sResultList = string.Join(", ", iResultList);
+        if (_bDebug) print($"Factor: {int.Parse(sN),3} | Iterations: {iResultList.Count,4} | results: {sResultList}");
+        if (iResultList.Contains(iTestValue)) return iTestValue;
+    }
+
+    return iReturnValue;
+
+}
+
+static List<ulong> UpdateResultList(List<ulong> _iInputResultList, ulong _iFactor)
+{
+
+    List<ulong> iOutputResultList = new List<ulong>();
+
+    if (_iInputResultList.Count == 0)
+    {
+        iOutputResultList.Add(_iFactor);
+    }
+    else
+    {
+        foreach (ulong i in _iInputResultList)
+        {
+            iOutputResultList.Add(i + _iFactor);
+            iOutputResultList.Add(i * _iFactor);
+        }
+    }
+
+    return iOutputResultList;
+
+}
+
+string sFilePath = "data.txt";
+// string sFilePath = "testdata.txt";
 
 List<string> sDataList = ReadFileToList(sFilePath);
 
+ulong iSum = 0;
+ulong iResult = 0;
+
 foreach (string sLine in sDataList)
 {
-    Console.WriteLine(sLine);
+    print("----------------------------------------------------------------------------------------------");
+    print(sLine);
+    iResult = CalculateString(sLine, false);
+    iSum += iResult;
+    print($"Line result: {iResult} | Acc result: {iSum}");
+    print("----------------------------------------------------------------------------------------------");
 }
+
+// int r = CalculateString("3267: 81 40 27");
+// int r = CalculateString("292: 11 6 16 20", true);
+
+print($"result 07a: {iSum}");
