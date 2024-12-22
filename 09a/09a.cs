c237b4be0c6deda2006e5ee2579c6a808f4fa6bb
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-22 12:13:41 +0100

    09a completed

--- 09a/09a.cs
+++ 09a/09a.cs
@@ -27,7 +27,7 @@ static string CompressDiskMap(string _sInputDiskMap)
     ixf = 0; // from the front
     ixb = _sInputDiskMap.Length - 1; // from the back
 
-    while (ixf <= ixb)
+    while (ixf < ixb)
     {
 
         if ((int)_sInputDiskMap[ixf] != (int)cEmpty)
@@ -43,7 +43,6 @@ static string CompressDiskMap(string _sInputDiskMap)
         }
 
         ixf++;
-        // print($"{ixf,5} | {ixb,5}");
 
     }
     return sOutputDiskMap;
@@ -59,9 +58,24 @@ static ulong CalculateChecksum(string _sString)
         iValue = (int)_sString[i];
         iChecksum = iChecksum + ((ulong)iValue * (ulong)i);
         // print($"{i,4} | Value: {iValue} | {(ulong)iValue * (ulong)i} | Checksum: {iChecksum}");
+
     }
 
     return iChecksum;
+}
+
+static int CountFileBlocks(string _sData)
+{
+    int cnt = 0;
+    char cEmpty = (char)65000;
+
+    foreach (char c in _sData)
+    {
+        if ((int)c != (int)cEmpty) cnt++;
+    }
+
+    return cnt;
+
 }
 
 string sFilePath = "data.txt";
@@ -93,13 +107,16 @@ foreach (char c in sData)
 
 }
 
-
 sCompressedDiskMap = CompressDiskMap(sDiskMap);
 iChecksum = CalculateChecksum(sCompressedDiskMap);
 
-print($"Indata:             {sData.Length}");
-print($"DiskMap:            {sDiskMap.Length}");
-print($"Compressed DiskMap: {sCompressedDiskMap.Length}");
-print($"Checksum:           {iChecksum}");
+print($"Indata:                {sData.Length}");
+print("--------------------------------------------------");
+print($"DiskMap:               {sDiskMap.Length}");
+print($"DiskMap fileblocks:    {CountFileBlocks(sDiskMap)}");
+print($"Compressed DiskMap:    {sCompressedDiskMap.Length}");
+print($"Compressed fileblocks: {CountFileBlocks(sCompressedDiskMap)}");
+print($"Checksum:              {iChecksum}");
 // 6604426148532 to low
 // 6607783130715 to high
+// 6607511583593 is the correct answer
