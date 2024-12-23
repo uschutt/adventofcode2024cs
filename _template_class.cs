c340680e9ea2b0c438e3a285d8e32caa585fce76
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-23 23:31:06 +0100

    new template

---
+++ _template_class.cs
@@ -0,0 +1,40 @@
+﻿class ProgramDDa
+{
+    static string __sFilePath = "testdata.txt";
+
+    static void Main(string[] args)
+    {
+        List<string> sDataList = ReadFileToList(__sFilePath);
+        foreach (string sLine in sDataList) print(sLine);
+    }
+
+    static string ReadFileToString(string _sFilePath)
+    {
+        string sReturnValue = "";
+
+        foreach (string line in File.ReadLines(_sFilePath))
+        {
+            sReturnValue += line.Trim();
+        }
+
+        return sReturnValue;
+    }
+
+    static List<string> ReadFileToList(string _sFilePath)
+    {
+        // Läs alla rader från filen
+        var sLinesList = new List<string>();
+
+        foreach (string sLine in File.ReadLines(_sFilePath))
+        {
+            sLinesList.Add(sLine.Trim());
+        }
+
+        return sLinesList;
+    }
+
+    static void print(string sText)
+    {
+        Console.WriteLine(sText);
+    }
+}
