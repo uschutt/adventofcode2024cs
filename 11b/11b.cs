98b35b07862e9dfa75c0e504578aa89eebedfb2b
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-24 21:33:19 +0100

    11b init

---
+++ 11b/11b.cs
@@ -0,0 +1,92 @@
+﻿class Program11b
+{
+    static string __sFilePath = "data.txt";
+    static int __iMaxLevel = 75;
+
+    static void Main(string[] args)
+    {
+        string sData = ReadFileToString(__sFilePath);
+        List<string> sDataList = sData.Split(' ').ToList();
+        int iBliks = 0;
+        int iTotalCount = 0;
+
+        iTotalCount = CountStones(sDataList);
+
+    }
+
+    static void PrintList(List<string> _sInputList, int _iX = 0)
+    {
+        // string sData = string.Join(' ', _sInputList);
+        string sData = "";
+        print($"{_iX,2} | {_sInputList.Count,8} | {sData} ");
+    }
+
+
+    static int CountStones(List<string> _sInputList, int _iLevel = 0, int _iTotalCount = 0)
+    {
+        List<string> sNewDataList = new List<string>();
+        int iHalfString;
+        ulong iNumber;
+        int iTotalCount = _iTotalCount;
+
+        PrintList(_sInputList, _iLevel);
+
+        foreach (string s in _sInputList)
+        {
+            if (s == "0")
+            {
+                sNewDataList.Add("1");
+            }
+            else if (s.Length % 2 == 0)
+            {
+                iHalfString = s.Length / 2;
+                sNewDataList.Add(IntifyString(s.Substring(0, iHalfString)));
+                sNewDataList.Add(IntifyString(s.Substring(s.Length - iHalfString, iHalfString)));
+            }
+            else
+            {
+                iNumber = ulong.Parse(s) * 2024;
+                sNewDataList.Add($"{iNumber}");
+            }
+
+        }
+
+        if (_iLevel == __iMaxLevel)
+        {
+            iTotalCount += sNewDataList.Count;
+        }
+        else
+        {
+            iTotalCount += CountStones(sNewDataList, _iLevel + 1, iTotalCount);
+
+        }
+
+        sNewDataList.Clear();
+
+        return iTotalCount;
+
+    }
+
+
+    static string IntifyString(string _sInput)
+    {
+        return $"{ulong.Parse(_sInput)}";
+    }
+
+    static string ReadFileToString(string _sFilePath)
+    {
+        string sReturnValue = "";
+
+        foreach (string line in File.ReadLines(_sFilePath))
+        {
+            sReturnValue += line.Trim();
+        }
+
+        return sReturnValue;
+    }
+
+    static void print(string sText)
+    {
+        Console.WriteLine(sText);
+    }
+}
