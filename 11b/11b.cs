79ff64744bf653a6feecafe1454be9aad5e6a7c7
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-25 18:41:27 +0100

    11b completed

--- 11b/11b.cs
+++ 11b/11b.cs
@@ -1,58 +1,95 @@
 ﻿using System;
 using System.Threading;
+using System.Collections.Generic;
 
 class Program11b
 {
     static string __sFilePath = "data.txt";
-    static int __iMaxLevel = 45;
-    static ulong __iTotalCount = 0;
+    static int __iMaxLevel = 75;
 
     static void Main(string[] args)
     {
         string sData = ReadFileToString(__sFilePath);
-        string[] sDataList = sData.Split(' ').ToArray();
+        ulong ulStoneCount = 0;
 
         DateTime dtStartTime = DateTime.Now;
 
-        foreach (string s in sDataList)
-        {
-            CountStones(s);
-        }
+        ulStoneCount = CountStones(sData);
 
         DateTime dtEndTime = DateTime.Now;
 
         TimeSpan tsDifference = dtEndTime - dtStartTime;
 
-        print($"Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds:  {__iTotalCount} stones ");
+        print($"Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds:  {ulStoneCount} stones ");
 
     }
 
-    static void CountStones(string _sStone, int _iLevel = 0)
+    static ulong CountStones(string _sStones)
     {
+        ulong ulTotalStoneCount = 0;
 
-        if (_iLevel == __iMaxLevel)
+        // create dictionarey with a string key (stone) and a int value
+        Dictionary<string, ulong> stones = new Dictionary<string, ulong>();
+
+        // init dictionary
+        foreach (string sStone in _sStones.Split(' ')) stones[sStone] = 1;
+
+        // for every level
+        for (int i = 0; i < __iMaxLevel; i++)
         {
-            __iTotalCount++;
+            // temporary dictionary to store new stones
+            Dictionary<string, ulong> newStones = new Dictionary<string, ulong>();
+
+            // step through previous set of stones
+            foreach (var stone in stones)
+            {
+                // use function StepStone to split current stone according to rules in StepStone function
+                foreach (string newStoneKey in StepStone(stone.Key))
+                {
+
+                    if (newStones.ContainsKey(newStoneKey))
+                    {
+
+                        newStones[newStoneKey] += stone.Value;
+                    }
+                    else
+                    {
+                        newStones[newStoneKey] = stone.Value;
+                    }
+                }
+            }
+
+            ulTotalStoneCount = 0;
+            foreach (ulong iStoneCount in newStones.Values) ulTotalStoneCount += iStoneCount;
+            print($"Level {i + 1,2} | stone count: {ulTotalStoneCount} ");
+
+            // result from each level replaces previously stored results
+            stones = newStones;
+
+        }
+
+        // sum of all stones
+        // foreach (int iStoneCount in stones.Values) ulTotalStoneCount += (ulong)iStoneCount;
+
+        return ulTotalStoneCount;
+
+    }
+
+    static string[] StepStone(string _sStone)
+    {
+        if (_sStone == "0")
+        {
+            return ["1"];
+        }
+        else if (_sStone.Length % 2 == 0)
+        {
+            int iHalfString = _sStone.Length / 2;
+            return [IntifyString(_sStone.AsSpan(0, iHalfString).ToString()), IntifyString(_sStone.AsSpan(_sStone.Length - iHalfString, iHalfString).ToString())];
         }
         else
         {
-            if (_sStone == "0")
-            {
-                CountStones("1", _iLevel + 1);
-            }
-            else if (_sStone.Length % 2 == 0)
-            {
-                int iHalfString = _sStone.Length / 2;
-                CountStones(IntifyString(_sStone.AsSpan(0, iHalfString).ToString()), _iLevel + 1);
-                CountStones(IntifyString(_sStone.AsSpan(_sStone.Length - iHalfString, iHalfString).ToString()), _iLevel + 1);
-
-            }
-            else
-            {
-                ulong iNumber = ulong.Parse(_sStone) * 2024;
-                CountStones(iNumber.ToString(), _iLevel + 1);
-            }
-
+            ulong iNumber = ulong.Parse(_sStone) * 2024;
+            return [iNumber.ToString()];
         }
     }
 
