7a9ae358b565f1fa5417343b219867cccef007f3
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-25 16:03:39 +0100

    11b still to slow

--- 11b/11b.cs
+++ 11b/11b.cs
@@ -4,8 +4,8 @@ using System.Threading;
 class Program11b
 {
     static string __sFilePath = "data.txt";
-    static int __iMaxLevel = 40;
-    static int __iTotalCount = 0;
+    static int __iMaxLevel = 45;
+    static ulong __iTotalCount = 0;
 
     static void Main(string[] args)
     {
@@ -19,7 +19,6 @@ class Program11b
             CountStones(s);
         }
 
-
         DateTime dtEndTime = DateTime.Now;
 
         TimeSpan tsDifference = dtEndTime - dtStartTime;
@@ -28,55 +27,37 @@ class Program11b
 
     }
 
-    static void PrintList(string _sStone, int _iX, int _iCount)
-    {
-        print($"{_iX,2} | {_iCount}");
-        // Thread.Sleep(10);
-    }
-
-
     static void CountStones(string _sStone, int _iLevel = 0)
     {
-        int iTotalCount = 1;
-        string[] sNewStoneArray;
 
         if (_iLevel == __iMaxLevel)
         {
-            __iTotalCount += iTotalCount;
+            __iTotalCount++;
         }
         else
         {
             if (_sStone == "0")
             {
-                sNewStoneArray = ["1"];
+                CountStones("1", _iLevel + 1);
             }
             else if (_sStone.Length % 2 == 0)
             {
                 int iHalfString = _sStone.Length / 2;
+                CountStones(IntifyString(_sStone.AsSpan(0, iHalfString).ToString()), _iLevel + 1);
+                CountStones(IntifyString(_sStone.AsSpan(_sStone.Length - iHalfString, iHalfString).ToString()), _iLevel + 1);
 
-                string leftHalf = IntifyString(_sStone.AsSpan(0, iHalfString).ToString()); // avoid using subsrings 
-                string rightHalf = IntifyString(_sStone.AsSpan(_sStone.Length - iHalfString, iHalfString).ToString()); // avoid using subsrings 
-
-                sNewStoneArray = [leftHalf, rightHalf];
-
-                iTotalCount = 2;
             }
             else
             {
                 ulong iNumber = ulong.Parse(_sStone) * 2024;
-                sNewStoneArray = [iNumber.ToString()];
+                CountStones(iNumber.ToString(), _iLevel + 1);
             }
 
-            foreach (string s in sNewStoneArray)
-            {
-                CountStones(s, _iLevel + 1);
-            }
         }
     }
 
     static string IntifyString(string _sInput)
     {
-        // return ulong.Parse(_sInput).ToString();
         string result = _sInput.TrimStart('0');
         return result == string.Empty ? "0" : result;
     }
