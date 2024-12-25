73efff0040431e97cce498afb988bbca4a14bef6
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-26 00:11:07 +0100

    11b solution comments and debug

--- 11b/11b.cs
+++ 11b/11b.cs
@@ -20,11 +20,13 @@ class Program11b
 
         TimeSpan tsDifference = dtEndTime - dtStartTime;
 
-        print($"Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds:  {ulStoneCount} stones ");
+        print("------------------------------------------------------------------------------------------------------------------------------------------");
+        print($"| Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds: {ulStoneCount} stones");
+        print("------------------------------------------------------------------------------------------------------------------------------------------");
 
     }
 
-    static ulong CountStones(string _sStones)
+    static ulong CountStones(string _sStones, bool _bDebug = false)
     {
         ulong ulTotalStoneCount = 0;
 
@@ -40,28 +42,46 @@ class Program11b
             // temporary dictionary to store new stones
             Dictionary<string, ulong> newStones = new Dictionary<string, ulong>();
 
+            if (_bDebug) print($"Blink {i + 1,2} -------------------------------------------------------------------------------------------------------------------------------");
+
             // step through previous set of stones
             foreach (var stone in stones)
             {
+
                 // use function StepStone to split current stone according to rules in StepStone function
+                // break down the stone and add new stones created by the StepStone function to the empty dict newStones
+                // the value added is the number of occurencies the current stone generates for the next run
                 foreach (string newStoneKey in StepStone(stone.Key))
                 {
-
                     if (newStones.ContainsKey(newStoneKey))
                     {
 
+                        if (_bDebug) PrintDebugRow(i, newStoneKey, newStones[newStoneKey], stone.Value, stone.Key);
+
+                        // for exampel: 
+                        // this level:  stone "2" exists 4 times i previous level
+                        //              rules in StepStone creates 4 stones with key "4048" (2*2024)
+                        // next level:  4 stones with key 4048 creates 4 stones with key "40" and another 4 stones with key "48"
                         newStones[newStoneKey] += stone.Value;
+
                     }
                     else
                     {
+
+                        if (_bDebug) PrintDebugRow(i, newStoneKey, 0, stone.Value, stone.Key);
+
                         newStones[newStoneKey] = stone.Value;
+
                     }
                 }
             }
 
+
             ulTotalStoneCount = 0;
             foreach (ulong iStoneCount in newStones.Values) ulTotalStoneCount += iStoneCount;
-            print($"Level {i + 1,2} | stone count: {ulTotalStoneCount} ");
+            if (_bDebug) print($"Blink {i + 1,2} -------------------------------------------------------------------------------------------------------------------------------");
+            if (_bDebug) print($"Blink {i + 1,2} | stone count: {ulTotalStoneCount,3}");
+            if (_bDebug) print("");
 
             // result from each level replaces previously stored results
             stones = newStones;
@@ -73,6 +93,29 @@ class Program11b
 
         return ulTotalStoneCount;
 
+    }
+
+    static void PrintDebugRow(int _iLevel, string _sNewStoneKey, ulong _ulNewCount, ulong _ulCurrentStoneCount, string _sCurrentStoneKey)
+    {
+
+        print($"Blink {_iLevel + 1,2} | {_sCurrentStoneKey,12} -> {_sNewStoneKey,12} | {_ulCurrentStoneCount,2} | Acc: {_ulNewCount + _ulCurrentStoneCount,2} | {_sCurrentStoneKey} exists {_ulCurrentStoneCount} time(s) in level {_iLevel}");
+        // print($"Blink {_iLevel + 1,2} | {_sCurrentStoneKey,12} -> {_sNewStoneKey,12} | {_ulNewCount,2} + {_ulCurrentStoneCount,2} = {_ulNewCount + _ulCurrentStoneCount,2} | {_sCurrentStoneKey} exists {_ulCurrentStoneCount} in level {_iLevel}");
+
+        // print($"Blink {_iLevel + 1,2} | {_sNewStoneKey,12} | {_ulNewCount,2} + {_ulCurrentStoneCount,2} = {_ulNewCount + _ulCurrentStoneCount,2} | stone {_sNewStoneKey} created from current stone {_sCurrentStoneKey} creates {_ulNewCount + _ulCurrentStoneCount} stones in level {_iLevel + 2} ");
+        // print($"Blink {i + 1,2} | old | {newStoneKey,12} |   {newStones[newStoneKey],2} + {stone.Value,2}          = {newStones[newStoneKey] + stone.Value,2} | stone          {newStoneKey} extracted from   ");
+    }
+
+    static string StoneDictToString(Dictionary<string, ulong> _inputStonesDict)
+    {
+        string sReturnValue = "";
+
+        SortedDictionary<string, ulong> sortedStones = new SortedDictionary<string, ulong>(_inputStonesDict);
+
+        foreach (var stone in sortedStones)
+        {
+            sReturnValue = $"{sReturnValue} | ({stone.Key},{stone.Value})";
+        }
+        return sReturnValue;
     }
 
     static string[] StepStone(string _sStone)
