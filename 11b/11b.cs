d0edde63c7595b8a26ed9a8a14d8c7b9c7fde1f4
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-25 17:31:43 +0100

    11b parallel not working

--- 11b/11b.cs
+++ 11b/11b.cs
@@ -6,25 +6,22 @@ using System.Threading.Tasks;
 class Program11b
 {
     static string __sFilePath = "data.txt";
-    static int __iMaxLevel = 25;
-    static int __iTotalCount = 0;
+    static int __iMaxLevel = 47;
+    static ConcurrentBag<int> results = new ConcurrentBag<int>();
 
     static void Main(string[] args)
     {
         string sData = ReadFileToString(__sFilePath);
         string[] sDataList = sData.Split(' ').ToArray();
-        int iTotalCount = 0;
-        ConcurrentBag<int> results = new ConcurrentBag<int>();
 
         DateTime dtStartTime = DateTime.Now;
 
-        // Parallel.ForEach(sDataList, (s) =>
-        // {
-        //     iTotalCount = CountStones([s]);
-        //     results.Add(iTotalCount);
-        // });
+        Parallel.ForEach(sDataList, (s) =>
+        {
+            CountStones(s);
+        });
 
-        iTotalCount = CountStones(sDataList);
+        // foreach (string s in sDataList) CountStones(s);
 
 
         DateTime dtEndTime = DateTime.Now;
@@ -32,8 +29,6 @@ class Program11b
         TimeSpan tsDifference = dtEndTime - dtStartTime;
 
         print($"Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds:  {results.Sum()} stones ");
-        print($"Total number of stones after {__iMaxLevel} blinks in {tsDifference.TotalSeconds} seconds:  {iTotalCount} stones ");
-
 
     }
 
@@ -44,52 +39,33 @@ class Program11b
     }
 
 
-    static int CountStones(string[] _sStoneArray, int _iLevel = 0)
+    static void CountStones(string _sStone, int _iLevel = 0)
     {
-        int iTotalCount = 1;
-        string[] sNewStoneArray;
 
-        foreach (string s in _sStoneArray)
+        if (_iLevel == __iMaxLevel)
         {
-
-            if (_iLevel == __iMaxLevel)
+            results.Add(1);
+        }
+        else
+        {
+            if (_sStone == "0")
             {
-                __iTotalCount += iTotalCount;
-                continue; // Exit early?
+                CountStones("1", _iLevel + 1);
             }
-
-            if (_iLevel < __iMaxLevel)
+            else if (_sStone.Length % 2 == 0)
             {
-                if (s == "0")
-                {
-                    sNewStoneArray = ["1"];
-                }
-                else if (s.Length % 2 == 0)
-                {
-                    int iHalfString = s.Length / 2;
-
-                    string leftHalf = IntifyString(s.AsSpan(0, iHalfString).ToString()); // avoid using subsrings 
-                    string rightHalf = IntifyString(s.AsSpan(s.Length - iHalfString, iHalfString).ToString()); // avoid using subsrings 
-
-                    sNewStoneArray = [leftHalf, rightHalf];
-
-                    iTotalCount = 2;
-                }
-                else
-                {
-                    ulong iNumber = ulong.Parse(s) * 2024;
-                    sNewStoneArray = [iNumber.ToString()];
-                }
-
-                iTotalCount += CountStones(sNewStoneArray, _iLevel + 1);
+                int iHalfString = _sStone.Length / 2;
+                CountStones(IntifyString(_sStone.AsSpan(0, iHalfString).ToString()), _iLevel + 1);
+                CountStones(IntifyString(_sStone.AsSpan(_sStone.Length - iHalfString, iHalfString).ToString()), _iLevel + 1);
 
             }
-
+            else
+            {
+                ulong iNumber = ulong.Parse(_sStone) * 2024;
+                CountStones(iNumber.ToString(), _iLevel + 1);
+            }
 
         }
-
-        return iTotalCount;
-
     }
 
 
