a0d5132c8564cb6360cd87fbd3a28dc9d2f85911
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-30 18:57:18 +0100

    13a under construction

--- 13a/13a.cs
+++ 13a/13a.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml;
 
 class Program13a
 {
@@ -30,9 +32,97 @@ class Program13a
         int iResult = 0;
 
         // solve the problem
+        // iResult = CalculateTotalCost(new Position(1, 1), new Position(8400, 5400), 94, 34, 3, 22, 67, 1);
+
+        iResult = CalculateCost();
 
         return iResult;
     }
+
+    static int CalculateCost()
+    {
+        Position _oInitPosition = new Position(0, 0);
+        Position _oiTargetPosition = new Position(8400, 5400);
+        Position oAMove = new Position(94, 34);
+        Position oBMove = new Position(22, 67);
+        Position oPosition = _oInitPosition;
+
+        int A = 0;
+        int B;
+
+        for (Position pA = _oInitPosition; pA < _oiTargetPosition; pA += oAMove)
+        {
+            A++;
+            B = 0;
+            for (Position pB = pA; pB < _oiTargetPosition; pB += oBMove)
+            {
+                B++;
+                oPosition = pA + pB;
+                print($"A: {A,3} | B: {B,3} | {oPosition.Description()}");
+                if (oPosition == _oiTargetPosition)
+                {
+                    print("This is it!");
+                    return 1;
+                }
+                if (oPosition > _oiTargetPosition) break;
+
+            }
+            if (oPosition > _oiTargetPosition) break;
+
+        }
+
+        return -1;
+
+        // Position p = GetPosition(new Position(1, 1), 1, 1, 94, 34);
+
+    }
+
+
+    static Position GetPosition(Position _oInitPosition, int _iXCount, int _iYCount, int _iXValue, int _iYValue)
+    {
+
+        Position oMove = new Position(_iXCount * _iXValue, _iYCount * _iYValue);
+        return _oInitPosition += oMove;
+
+    }
+
+
+    // alla kombinationer av antal tryck på a från 1 till 89
+
+    // static int CalculateTotalCost(Position _oInitPosition, Position _oiTargetPosition, int _iAXValue, int _iAYValue, int _iACost, int _iBXValue, int _iBYValue, int _iBCost)
+    // {
+
+    //     int x = _oInitPosition.x;
+    //     int y = _oInitPosition.y;
+    //     Position p = _oInitPosition;
+
+    //     int iXCount = 0;
+    //     int iYCount = 0;
+
+    //     int iAXMax = (_oiTargetPosition.x - _oInitPosition.x) / _iAXValue;
+    //     int iAYMax = (_oiTargetPosition.y - _oInitPosition.y) / _iAYValue;
+
+    //     int iBXMax = (_oiTargetPosition.x - _oInitPosition.x) / _iBXValue;
+    //     int iBYMax = (_oiTargetPosition.y - _oInitPosition.y) / _iBYValue;
+
+    //     for (int ax = 1; ax < 89; ax++)
+    //     {
+    //         for (int ay = 1; ay <= 158; ax++)
+    //         {
+    //             for (int bx = 1; bx < 381; bx++)
+    //             {
+    //                 for (int by = 1; by <= 80; ax++)
+    //                 {
+    //                     x += ax * _iAXValue;
+    //                     y += ay * _iAYValue;
+    //                     p = new Position(x,y)
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return iAXMax;
+    // }
 
     static string ReadFileToString(string _sFilePath)
     {
---
+++ 13a/Position.cs
@@ -0,0 +1,110 @@
+class Position
+{
+    public int x;
+    public int y;
+    public string name;
+
+    public Position(int _x, int _y)
+    {
+        x = _x; // horisontal / col / string
+        y = _y; // vertical   / row / item
+        name = "";
+    }
+
+    public Position(int _x, int _y, string _name)
+    {
+        x = _x; // horisontal / col / string
+        y = _y; // vertical   / row / item
+        name = _name;
+    }
+
+
+
+    public static Position operator -(Position obj1, Position obj2)
+    {
+        int x, y;
+
+        x = obj1.x - obj2.x;
+        y = obj1.y - obj2.y;
+
+        return new Position(x, y);
+    }
+
+    public static Position operator +(Position obj1, Position obj2)
+    {
+        int x, y;
+
+        x = obj1.x + obj2.x;
+        y = obj1.y + obj2.y;
+
+        return new Position(x, y);
+    }
+
+    public static bool operator <=(Position obj1, Position obj2)
+    {
+        return obj1.x <= obj2.x && obj1.y <= obj2.y;
+    }
+
+    public static bool operator >=(Position obj1, Position obj2)
+    {
+        return obj1.x >= obj2.x && obj1.y >= obj2.y;
+    }
+
+    public static bool operator <(Position obj1, Position obj2)
+    {
+        return obj1.x < obj2.x && obj1.y < obj2.y;
+    }
+
+    public static bool operator >(Position obj1, Position obj2)
+    {
+        return obj1.x > obj2.x && obj1.y > obj2.y;
+    }
+
+    public static bool operator ==(Position obj1, Position obj2)
+    {
+        if (ReferenceEquals(obj1, obj2))
+            return true; // Both references point to the same object
+        if (obj1 is null || obj2 is null)
+            return false; // One is null, and the other is not
+
+        // Compare property values
+        return obj1.x == obj2.x && obj1.y == obj2.y;
+    }
+
+    public static bool operator !=(Position obj1, Position obj2)
+    {
+        return !(obj1 == obj2);
+    }
+
+    // frågetecknet(?) efter object markerar att obj kan vara null
+    public override bool Equals(object? obj)
+    {
+        if (obj is Position other)
+        {
+            return this == other;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(x, y);
+    }
+
+    public void print(string _prefix = "")
+    {
+        string sText = Description();
+        if (_prefix != "") sText = $"{_prefix}{sText}";
+        Console.WriteLine(sText);
+    }
+
+    public string Description()
+    {
+        if (name != "") return $"({x,3},{y,3}) {name}";
+
+        return $"({x,3},{y,3})";
+    }
+
+    public override string ToString() => Description();
+
+}
