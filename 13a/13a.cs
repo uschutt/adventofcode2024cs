c1c7032873ab1e31a2b42d4ddbd5a232c432308e
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-30 16:37:36 +0100

    13a init

---
+++ 13a/13a.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+class Program13a
+{
+    static string __sFilePath = "testdata.txt";
+
+    static void Main(string[] args)
+    {
+        int iResult;
+
+        List<string> sDataList = ReadFileToList(__sFilePath);
+        foreach (string sLine in sDataList) print(sLine);
+
+        DateTime dtStartTime = DateTime.Now;
+
+        iResult = Solution(sDataList);
+
+        DateTime dtEndTime = DateTime.Now;
+
+        TimeSpan tsDifference = dtEndTime - dtStartTime;
+
+        print("------------------------------------------------------------------------------------------------------------------------------------------");
+        print($"| Result: {iResult} | calculated in {tsDifference.TotalSeconds} seconds");
+        print("------------------------------------------------------------------------------------------------------------------------------------------");
+
+    }
+
+    static int Solution(List<string> _sDataList)
+    {
+        int iResult = 0;
+
+        // solve the problem
+
+        return iResult;
+    }
+
+    static string ReadFileToString(string _sFilePath)
+    {
+        string sReturnValue = "";
+
+        foreach (string line in File.ReadLines(_sFilePath))
+        {
+            sReturnValue += line.Trim();
+        }
+
+        return sReturnValue;
+    }
+
+    static List<string> ReadFileToList(string _sFilePath)
+    {
+        // Läs alla rader från filen
+        var sLinesList = new List<string>();
+
+        foreach (string sLine in File.ReadLines(_sFilePath))
+        {
+            sLinesList.Add(sLine.Trim());
+        }
+
+        return sLinesList;
+    }
+
+    static void print(string sText)
+    {
+        Console.WriteLine(sText);
+    }
+}
