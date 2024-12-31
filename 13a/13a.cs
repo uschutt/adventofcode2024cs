64d0a303890501e6038b31022a469f752d9c7c76
Ulf Schütt <ulf.schutt@gmail.com>
2024-12-31 14:44:09 +0100

    13a completed

--- 13a/13a.cs
+++ 13a/13a.cs
@@ -1,15 +1,17 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 class Program13a
 {
-    static string __sFilePath = "testdata.txt";
+    static string __sFilePath = "data.txt";
 
     static void Main(string[] args)
     {
         int iResult;
 
         List<string> sDataList = ReadFileToList(__sFilePath);
-        foreach (string sLine in sDataList) print(sLine);
+        // foreach (string sLine in sDataList) print(sLine);
 
         DateTime dtStartTime = DateTime.Now;
 
@@ -28,36 +30,56 @@ class Program13a
     static int Solution(List<string> _sDataList)
     {
         int iResult = 0;
+        Position oInitPosition = new Position(0, 0);
+        List<MachineSettings> MachineList = new List<MachineSettings>();
+        List<string> sMachineInputList = new List<string>();
 
-        // solve the problem
-        iResult = CalculateCost();
+        foreach (string inputLine in _sDataList)
+        {
+
+            if (inputLine.Trim() == "")
+            {
+                MachineList.Add(new MachineSettings(sMachineInputList));
+                sMachineInputList = new List<string>();
+            }
+            else
+            {
+                sMachineInputList.Add(inputLine);
+            }
+        }
+
+        MachineList.Add(new MachineSettings(sMachineInputList));
+
+        foreach (MachineSettings ms in MachineList)
+        {
+            iResult += CalculateCost(oInitPosition, ms.TargetPosition, ms.ButtonAMove, ms.ButtonBMove);
+        }
 
         return iResult;
     }
 
-    static int CalculateCost()
+    static int CalculateCost(Position _oInitPosition, Position _oiTargetPosition, Position _oAMove, Position _oBMove)
     {
-        Position _oInitPosition = new Position(0, 0);
-        Position _oiTargetPosition = new Position(8400, 5400);
-        Position _oAMove = new Position(94, 34);
-        Position _oBMove = new Position(22, 67);
-        Position oPosition = _oInitPosition;
+        Position oPosition;
 
-        int A = 0;
-        int B;
+        int A, B;
+        int iMinimumCost = 0;
+        ClawMoves newClawMoves;
+        List<ClawMoves> winningClawMovesList = new List<ClawMoves>();
 
+        A = 0;
         for (Position pA = _oInitPosition; pA < _oiTargetPosition; pA += _oAMove)
         {
+            if (A > 100) break;
             B = 0;
             for (Position pB = _oInitPosition; pB < _oiTargetPosition; pB += _oBMove)
             {
-
+                if (B > 100) break;
                 oPosition = pA + pB;
-                print($"A: {A,3} {pA.Description()} | B: {B,3} {pB.Description()} | {oPosition.Description()}");
                 if (oPosition == _oiTargetPosition)
                 {
-                    print("This is it!");
-                    return 1;
+                    newClawMoves = new ClawMoves(3, 1, A, B, oPosition);
+                    winningClawMovesList.Add(newClawMoves);
                 }
                 if (pB > _oiTargetPosition) break;
                 B++;
@@ -66,20 +88,13 @@ class Program13a
             A++;
         }
 
-        return -1;
-
-    }
-
-    static string ReadFileToString(string _sFilePath)
-    {
-        string sReturnValue = "";
-
-        foreach (string line in File.ReadLines(_sFilePath))
+        foreach (ClawMoves cm in winningClawMovesList)
         {
-            sReturnValue += line.Trim();
+            if (iMinimumCost == 0 || cm.Cost < iMinimumCost) iMinimumCost = cm.Cost;
         }
 
-        return sReturnValue;
+        return iMinimumCost;
+
     }
 
     static List<string> ReadFileToList(string _sFilePath)
@@ -99,4 +114,70 @@ class Program13a
     {
         Console.WriteLine(sText);
     }
+}
+
+class ClawMoves
+{
+    private int __ButtonCostA;
+    private int __ButtonCostB;
+    private int __ButtonClickCountA;
+    private int __ButtonClickCountB;
+    private Position __TargetPosition;
+    private Position __InitPosition;
+
+    public ClawMoves(int _iButtonCostA, int _iButtonCostB, int _iButtonClickCountA, int _iButtonClickCountB, Position _oTargetPosition)
+    {
+        __ButtonCostA = _iButtonCostA;
+        __ButtonCostB = _iButtonCostB;
+        __ButtonClickCountA = _iButtonClickCountA;
+        __ButtonClickCountB = _iButtonClickCountB;
+        __InitPosition = new Position(0, 0);
+        __TargetPosition = _oTargetPosition;
+    }
+
+    public int Cost
+    {
+        get { return (__ButtonClickCountA * __ButtonCostA) + (__ButtonClickCountB * __ButtonCostB); }
+    }
+}
+
+class MachineSettings
+{
+    private Position __ButtonAMove;
+    private Position __ButtonBMove;
+    private Position __TargetPosition;
+
+    public Position ButtonAMove
+    {
+        get { return __ButtonAMove; }
+    }
+
+    public Position ButtonBMove
+    {
+        get { return __ButtonBMove; }
+    }
+
+    public Position TargetPosition
+    {
+        get { return __TargetPosition; }
+    }
+
+    public MachineSettings(List<string> __sMachineSettingsList)
+    {
+        __ButtonAMove = StringToPosition(__sMachineSettingsList[0]);
+        __ButtonBMove = StringToPosition(__sMachineSettingsList[1]);
+        __TargetPosition = StringToPosition(__sMachineSettingsList[2]);
+    }
+
+    private Position StringToPosition(string _sInput)
+    {
+        string sInput = _sInput.Replace('+', '=');
+        string[] sCoordinatesArray = sInput.Split(':')[1].Split(',');
+        string sX = sCoordinatesArray[0].Replace("X=", "").Trim();
+        string sY = sCoordinatesArray[1].Replace("Y=", "").Trim();
+        int iX = int.Parse(sX);
+        int iY = int.Parse(sY);
+        return new Position(iX, iY);
+    }
+
 }
